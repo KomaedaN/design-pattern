4023d7aaaa52984e66d70fbc07a1889cea9969a2
Thomas <thomasmrt200@gmail.com>
2026-07-25 16:24:03 +0200

    ajout du sujet

--- src/core/builder.ts
+++ src/core/builder.ts
@@ -1,19 +1,35 @@
-class AppConfig {
+export class AppConfig {
   private static instance?: AppConfig;
-  public readonly appTitle: string;
-  public readonly pageTitle: string;
+  private appTitle: string;
+  private pageTitle: string;
 
   private constructor(appTitle: string, pageTitle: string) {
     this.appTitle = appTitle;
     this.pageTitle = pageTitle;
   }
 
-  public static getInstance(): AppConfig {
+  private static getInstance(): AppConfig {
     if (AppConfig.instance) {
       return AppConfig.instance;
     }
 
     AppConfig.instance = new AppConfig("League Of Legends", "Home Page");
     return AppConfig.instance;
+  }
+
+  public static setAppTitle(title: string): void {
+    AppConfig.getInstance().appTitle = title;
+  }
+
+  public static setPageTitle(title: string): void {
+    AppConfig.getInstance().pageTitle = title;
+  }
+
+  public static getAppTitle() {
+    return AppConfig.getInstance().appTitle;
+  }
+
+  public static getPageTitle() {
+    return AppConfig.getInstance().pageTitle;
   }
 }
